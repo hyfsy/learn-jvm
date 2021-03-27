dcf31ccda6d0bf760e1cf529f06d1aa87df599ab
hyfsy <1577975140@qq.com>
2021-03-27 11:23:15 +0800

    初始化仓库

---
+++ src/test/java/com/hyf/jvm/Hello.java
@@ -0,0 +1,16 @@
+package com.hyf.jvm;
+
+import org.junit.Test;
+
+/**
+ * @author baB_hyf
+ * @date 2021/03/25
+ */
+public class Hello {
+
+    @Test
+    public void test1() {
+        Object o = new Object();
+    }
+
+}
