4019cdac21c766d7acd65774632c309623c5e5c1
hyfsy <1577975140@qq.com>
2021-05-04 12:04:22 +0800

    asm注释添加

---
+++ src/test/java/com/hyf/bytecode/asm/Main.java
@@ -0,0 +1,13 @@
+package com.hyf.bytecode.asm;
+
+/**
+ * 文档：https://asm.ow2.io/asm4-guide.pdf
+ * <p>
+ * idea插件：asm bytecode outline.
+ * 点击 java文件，右击 show bytecode outline就可以查看到asm自动生成的代码了
+ *
+ * @author baB_hyf
+ * @date 2021/04/30
+ */
+public class Main {
+}
